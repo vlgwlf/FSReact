4b07094feea20bf732c2869354a74c3d8ce7171a
Javier Villalobos <javier_abraham_v@outlook.com>
2018-03-16 10:57:07 -0700

    Fixed typo in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // keys.js - figure out what set of credentials to use
 if (process.env.NODE_ENV === 'production') {
     // in production, return prod keys
-    module.exports(require('./prod'));
+    module.exports = require('./prod');
 } else {
     // in dev, return dev keys
     module.exports = require('./dev');
